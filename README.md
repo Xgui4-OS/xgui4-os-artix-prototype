f58e8c602412a3009a2a966453f209b2208fe6e6
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-08-08 23:51:12 -0400

    first prototype ever (BIG WIP)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Xgui4 OS Artix Protoype
+
+This is my first prototype of Xgui4 OS using Artix as a base
---
+++ profiles/xgui4-os-default/root-overlay/etc/skel/.config/autostart/wine-config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd ${WINEPREFIX:-~/.wine}/drive_c/windows/Fonts && for i in /usr/share/fonts/**/*.{ttf,otf}; do ln -s "$i"; done
+winetricks fontsmooth=rgb &
+
+rm -f ~/.config/autostart/wine-config.*
